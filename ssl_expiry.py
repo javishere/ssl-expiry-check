fc985dbfbbd6ee4089d06fcdf44e32cfc90f0250
Javier Hortal Amador <jhortal@hiberus.com>
2020-10-28 23:55:44 +0100

    Version 1.0

--- ssl_expiry.py
+++ ssl_expiry.py
@@ -17,66 +17,74 @@ FILE_NAME_TXT=f'ssl-checker-{dt}.txt'
 FILE_NAME_CSV=f'ssl-checker-{dt}.csv'
 TABLE_HEADER = ["hostname", "IP-host","expire_date", "from", "organization", "cert" , "expire_in", "connectionInfo"]
 
-logger = logging.getLogger('SSLVerify')
+
 checker_writer = PrettyTable()
 
-def ssl_infomation(hostname: str) -> [datetime.datetime, datetime.datetime, str, str]:
+def host_information(hostname: str) -> [datetime.datetime, datetime.datetime, str, str]:
     ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
+    host_info = {       "ip" : '-',
+                        "expire-at": '-', 
+                        "from": '-', 
+                        "organization":'-', 
+                        "cert":  '-',
+                        "will-expire-in": '-',
+                        "output": '-'
+                }
 
     context = ssl.create_default_context()
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_OPTIONAL
     conn = context.wrap_socket( socket.socket(socket.AF_INET),server_hostname=hostname,)
     # 3 second timeout because Lambda has runtime limitations
-    conn.settimeout(3.0)
+    conn.settimeout(1.5)
+    
+    
+    try:
+        conn.connect_ex((hostname, 443))
+        host_info["ip"]=conn.getpeername()
+        ssl_info = conn.getpeercert()
+    except socket.timeout as e:
+        host_info["output"] = f'Time-out '
+    except Exception as e:
+        host_info["output"] = f'ERROR  {e}'
+    else:
+        host_info["expire-at"]= datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
+        host_info["from"]=datetime.datetime.strptime(ssl_info['notBefore'], ssl_date_fmt)       
+        host_info["will-expire-in"]=ssl_valid_time_remaining(host_info["expire-at"])
+        host_info["organization"]=ssl_info["subject"][0][0][1]
+        host_info["cert"]=ssl_info["issuer"][2][0][1] if "commonName" in ssl_info["issuer"][2][0] else ssl_info["issuer"][3][0][1]
+        
+        try:
+            ssl.match_hostname(ssl_info, hostname)
+        except ssl.CertificateError as e: 
+            host_info["output"] = f'ERROR  {e}'
+        else:
+            host_info["output"] = "OK"
+    finally:
+        # parse the string from the certificate into a Python datetime object
+        return host_info
 
-    logger.debug('Connect to {}'.format(hostname))
-    conn.connect((hostname, 443))
-    ssl_info = conn.getpeercert()
-    ssl_name = conn.getpeername()
-    # parse the string from the certificate into a Python datetime object
-    return [ssl_name[0],
-            datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt), 
-            datetime.datetime.strptime(ssl_info['notBefore'], ssl_date_fmt), 
-            ssl_info["subject"][0][0][1], 
-            ssl_info["issuer"][3][0][1]]
 
 
 def ssl_valid_time_remaining(expires) -> datetime.timedelta:
     """Get the number of days left in a cert's lifetime."""
-    # logger.debug(
-    #     'SSL cert for {} expires at {}'.format(
-    #         hostname, expires.isoformat()
-    #     )
-    # )
     return expires - datetime.datetime.utcnow()
 
 
-def test_host(hostname: str, buffer_days: int=30): # -> [ssl_inf,will_expire_in, fromDate, commonName, "-"]:
-    """Return test message for hostname cert expiration."""
-    ssl_info=['-']*5
-    will_expire_in = '-'
-    error = '-'
+def print_output(hostname, host_info, buffer_days: int=30):
+    will_expire_in = host_info["will-expire-in"]
+    output = hostname + " " + host_info["output"]
     try:
-        ssl_info = ssl_infomation(hostname)
-        will_expire_in = ssl_valid_time_remaining(ssl_info[1])
-    except ssl.CertificateError as e:
-        output = f'{hostname} cert error {e}' 
-    except ssl.SSLError as e:
-        output = f'{hostname} SSL error {e}'
-    except socket.timeout as e:
-        output = f'{hostname} Time-out '
-    except Exception as e:
-        output = f'{hostname} ERROR  {e}'
-    else:
         if will_expire_in < datetime.timedelta(days=0):
-           output = f'{hostname} cert expired'
+            output = f'{hostname} cert expired'
         elif will_expire_in < datetime.timedelta(days=buffer_days):
             output = f'{hostname} cert will expire in {will_expire_in}'
         else:
             output = f'{hostname} cert is fine'
-
+    except Exception:
+        pass
     finally:
-        print(output)
-        return [*ssl_info, will_expire_in, output]
+        print(output)    
 
 def make_csv(fieldnames, data):
     with open(FILE_NAME_CSV,"w", newline='') as ssl_table:
@@ -86,25 +94,18 @@ def make_csv(fieldnames, data):
             ssl_writer.writerow(row)
 
 
-if __name__ == '__main__':
-    loglevel = os.environ.get('LOGLEVEL', 'INFO')
-    numeric_level = getattr(logging, loglevel.upper(), None)
-    if not isinstance(numeric_level, int):
-        raise ValueError('Invalid log level: %s' % loglevel)
-    logging.basicConfig(level=numeric_level)
-    
+if __name__ == '__main__':    
 
     start = time.time()
     table_ssl = []
     for host in fileinput.input():
         host = host.strip()
-        logger.debug('Testing host {}'.format(host))
-        ssl_info= test_host(host)
-        checker_writer.add_row([host, *ssl_info])
-        table_ssl.append([host, *ssl_info])
+        ssl_info= host_information(host)
+        print_output(host, ssl_info)
+        checker_writer.add_row([host, *list(ssl_info.values())])
+        table_ssl.append([host, *list(ssl_info.values())])
 
     
     make_csv(TABLE_HEADER,table_ssl)
     checker_writer.field_names=TABLE_HEADER
     print(checker_writer)
-    logger.debug('Time: {}'.format(time.time() - start))
